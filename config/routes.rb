e5be51935d939eb8a29e5c40cc2eac6ce7a57318
Chris Bon <chrisbon315@gmail.com>
2016-05-19 01:08:16 -0700

    Change root route to application#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'musicians#index'
+  root 'application#home'
+  resources :musicians
 
-  #     new_user_session  GET     /users/sign_in   devise/sessions#new
-  #         user_session  POST    /users/sign_in   devise/sessions#create
-  # destroy_user_session  DELETE  /users/sign_out  devise/sessions#destroy
+  #     new_user_session GET    /users/sign_in  devise/sessions#new
+  #         user_session POST   /users/sign_in  devise/sessions#create
+  # destroy_user_session DELETE /users/sign_out devise/sessions#destroy
 
-  #      user_password POST  /users/password       devise/passwords#create
-  #  new_user_password GET   /users/password/new   devise/passwords#new
-  # edit_user_password GET   /users/password/edit  devise/passwords#edit
-  #                    PATCH /users/password       devise/passwords#update
+  #      user_password POST  /users/password      devise/passwords#create
+  #  new_user_password GET   /users/password/new  devise/passwords#new
+  # edit_user_password GET   /users/password/edit devise/passwords#edit
+  #                    PATCH /users/password      devise/passwords#update
 
   # cancel_user_registration GET    /users/cancel  devise/registrations#cancel
   #        user_registration POST   /users         devise/registrations#create
