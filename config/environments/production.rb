b7f39c761cec2b795fb6af5e3812fb37f00688d0
Chris Bon <chrisbon315@gmail.com>
2016-06-14 14:10:51 -0700

    edit url on readme

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = 
-    { host: 'fathomless-ocean-61330.herokuapp.com' }
+    { host: 'circlejam.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
 
