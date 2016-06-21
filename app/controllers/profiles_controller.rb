4ffa55474349815d1f003da935fa4d450d2b0cce
Chris Bon <chrisbon315@gmail.com>
2016-06-21 12:28:26 -0700

    prompt testing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,34 +1,34 @@
 class ProfilesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :set_profile, only: [:show, :hide, :edit, :update, :destroy]
 
   # GET /profiles
   def index
     @profiles = Profile.all
     profiles = []
     if params[:name]
-      Profile.all.each do |p| 
+      @profiles.each do |p| 
         if p.name.include? params[:name]
           profiles << p
         end
       end
     end
     if params[:age]
-      Profile.all.each do |p|
+      @profiles.each do |p|
         unless profiles.include?(p) || p.age != params[:age]
           profiles << p
         end
       end
     end
     if params[:location]
-      Profile.all.each do |p|
+      @profiles.each do |p|
         unless profiles.include?(p) || !p.location.include?(params[:location])
           profiles << p
         end
       end
     end
     if params[:genres]
-      Profile.all.each do |p|
+      @profiles.each do |p|
         unless profiles.include?(p) || !p.genres.include?(params[:genres])
           profiles << p
         end
@@ -36,7 +36,7 @@ class ProfilesController < ApplicationController
     end
     if params[:availability]
       days = params[:availability].downcase.split(',').map! &:strip  
-      Profile.all.each do |p|
+      @profiles.each do |p|
         unless profiles.include? p
           days.each do |day| 
             if p.include? day
@@ -52,26 +52,35 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/:id
   def show
+    redirect_to '/profiles/unshow' unless signed_in?
+
+    if signed_in? && @profile.user != current_user
+      redirect_to "/profiles/#{params[:id]}/hide" 
+    end
+
+    if signed_in? && @profile.user == current_user
+      redirect_to "/profiles/#{params[:id]}/edit"
+    end
+  end
+
+  # GET /profiles/hide
+  def hide
   end
 
   # GET /profiles/new
   def new
     @profile = Profile.new
-
   end
 
   # POST /profiles
   def create
-    @profile = Profile.new    name: params[:name],      
-                               age: params[:age],   
-                            gender: params[:gender],  
-                      phone_number: params[:phone_number], 
-                             email: params[:email],
-                          location: params[:location],
-                       instruments: params[:instruments].downcase,
-                             genre: params[:genre],
-                      availability: params[:availability], 
-                           user_id: current_user.id 
+    @profile = Profile.new       user_id: current_user.id, 
+      name: params[:name],       age: params[:age], 
+      gender: params[:gender],   phone_number: params[:phone_number], 
+      email: params[:email],     location: params[:location], 
+      privacy: params[:privacy], genre: params[:genre], 
+      availability: params[:availability],
+      instruments: params[:instruments].downcase
     current_user.update profile_id: @profile.id
     respond_to do |format|
       if @profile.save
@@ -91,15 +100,12 @@ class ProfilesController < ApplicationController
 
   # PATCH /profiles/:id
   def update
-    post_params = { name: params[:name],      
-                     age: params[:age],   
-                  gender: params[:gender],  
-            phone_number: params[:phone_number], 
-                   email: params[:email],
-             instruments: params[:instruments].downcase,
-                   genre: params[:genre],
-            availability: params[:availability], 
-                 user_id: current_user.id }
+    post_params = {            user_id: current_user.id,            
+      name: params[:name],     age: params[:age],          
+      gender: params[:gender], phone_number: params[:phone_number],
+      email: params[:email],   genre: params[:genre],
+      availability: params[:availability], 
+      instruments: params[:instruments].downcase }
     respond_to do |format|
       if @profile.update post_params
         format.html { redirect_to @post, notice: 'Profile updated!' }
@@ -107,7 +113,7 @@ class ProfilesController < ApplicationController
       else
         format.html { render :edit }
         format.json { render json: @profile.errors,
-                           status: :unprocessable_entity }
+                             status: :unprocessable_entity }
       end
     end
   end
