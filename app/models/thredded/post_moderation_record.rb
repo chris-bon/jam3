b526052197714c08b33ec558cc8662726ed71d47
Chris Bon <chrisbon315@gmail.com>
2016-06-20 21:21:04 -0700

    re-edit PostModerationRecord model file

--- app/models/thredded/post_moderation_record.rb
+++ app/models/thredded/post_moderation_record.rb
@@ -4,9 +4,9 @@ module Thredded
     include ModerationState
     # Rails 4 doesn't support enum _prefix
     if Rails::VERSION::MAJOR >= 5
-      enum(previous_moderation_state: moderation_states, _prefix: :previous)
+      enum previous_moderation_state: moderation_states, _prefix: :previous
     end
-   # validates :previous_moderation_state, presence: true
+    # validates :previous_moderation_state, presence: true
 
     scope :order_newest_first, -> { order(created_at: :desc, id: :desc) }
 
@@ -16,17 +16,17 @@ module Thredded
     belongs_to :post_user, class_name: Thredded.user_class, inverse_of: :thredded_post_moderation_records
     belongs_to :moderator, class_name: Thredded.user_class, inverse_of: :thredded_post_moderation_records
 
-   validates_each :moderation_state do |record, attr, value|
-     if record.previous_moderation_state == value
-       record.errors.add(attr, "Post moderation_state is already #{value}")
-     end
-   end
+   # validates_each :moderation_state do |record, attr, value|
+   #   if record.previous_moderation_state == value
+   #     record.errors.add attr, "Post moderation_state is already #{value}"
+   #   end
+   # end
 
-    @param [Thredded.user_class] moderator
-    @param [Thredded::Post] post
-    @param [Symbol, String] previous_moderation_state
-    @param [Symbol, String] moderation_state
-    @return [Thredded::PostModerationRecord] the newly created persisted record
+    # @param([Thredded.user_class] moderator)
+    # @param([Thredded::Post] post)
+    # @param [Symbol, String] previous_moderation_state
+    # @param [Symbol, String] moderation_state
+    # @return [Thredded::PostModerationRecord] the newly created persisted record
     def self.record!(moderator:, post:, previous_moderation_state:, moderation_state:)
       # Rails 4 doesn't support enum _prefix
       if Rails::VERSION::MAJOR < 5
@@ -34,13 +34,13 @@ module Thredded
       end
       create!(
         previous_moderation_state: previous_moderation_state,
-        moderation_state:          moderation_state,
-        moderator:                 moderator,
-        post:                      post,
-        post_content:              post.content,
-        post_user:                 post.user,
-        post_user_name:            post.user.try(:send, Thredded.user_name_column),
-        messageboard_id:           post.messageboard_id,
+        moderation_state: moderation_state,
+        moderator:        moderator,
+        post:             post,
+        post_content:     post.content,
+        post_user:        post.user,
+        post_user_name:   post.user.try(:send, Thredded.user_name_column),
+        messageboard_id:  post.messageboard_id,
       )
     end
   end
